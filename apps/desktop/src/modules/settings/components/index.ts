7a3fc3968ac9d13f20946a394f89d0ec0edb545d
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-18 23:35:45 +0300

    refactor: export separate local data dialogs

--- apps/desktop/src/modules/settings/components/index.ts
+++ apps/desktop/src/modules/settings/components/index.ts
@@ -3,6 +3,11 @@ export { InstructionSettingsSection } from "./InstructionSettingsSection";
 export { BackupSettingsSection } from "./BackupSettingsSection";
 export { CoreContentSection } from "./CoreContentSection";
 export { LocalDataControlsSection } from "./LocalDataControlsSection";
+export {
+  ResetApplicationDialog,
+  SelectiveDataRemovalDialog,
+  type LocalDataCategoryOption
+} from "./LocalDataRemovalDialogs";
 export { SettingsCategoryNavigation } from "./SettingsCategoryNavigation";
 export {
   SettingsMaintenanceOverview,
