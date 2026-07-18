f9d665b22bb4b010d6b32b8ab2d8f48afde48129
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-18 23:39:58 +0300

    refactor: keep dialog internals private

--- apps/desktop/src/modules/settings/components/index.ts
+++ apps/desktop/src/modules/settings/components/index.ts
@@ -3,11 +3,6 @@ export { InstructionSettingsSection } from "./InstructionSettingsSection";
 export { BackupSettingsSection } from "./BackupSettingsSection";
 export { CoreContentSection } from "./CoreContentSection";
 export { LocalDataControlsSection } from "./LocalDataControlsSection";
-export {
-  ResetApplicationDialog,
-  SelectiveDataRemovalDialog,
-  type LocalDataCategoryOption
-} from "./LocalDataRemovalDialogs";
 export { SettingsCategoryNavigation } from "./SettingsCategoryNavigation";
 export {
   SettingsMaintenanceOverview,
