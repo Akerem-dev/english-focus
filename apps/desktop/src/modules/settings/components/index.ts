c7c072498f54a415b55b8ffd5f6db6ab21956d47
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-18 17:57:52 +0300

    fix: close premium settings quality findings

--- apps/desktop/src/modules/settings/components/index.ts
+++ apps/desktop/src/modules/settings/components/index.ts
@@ -1,9 +1,7 @@
-export { ActivitySection } from "./ActivitySection";
 export { InstructionSettingsSection } from "./InstructionSettingsSection";
 
 export { BackupSettingsSection } from "./BackupSettingsSection";
 export { CoreContentSection } from "./CoreContentSection";
-export { DiagnosticsSection } from "./DiagnosticsSection";
 export { LocalDataControlsSection } from "./LocalDataControlsSection";
 export {
   SettingsCategoryNavigation,
