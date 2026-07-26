393dfb07a78b832949206014926077cb550ed1dd
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-26 20:32:01 +0300

    feat(assistant): export review and provider boundary

---
+++ apps/desktop/src/modules/assistant/application/index.ts
@@ -0,0 +1,8 @@
+export {
+  inspectAssistantCandidate,
+  type AssistantCandidateReview
+} from "./InspectAssistantCandidate";
+export {
+  prepareAssistantWord,
+  type AssistantWordPreparationResult
+} from "./PrepareAssistantWord";
