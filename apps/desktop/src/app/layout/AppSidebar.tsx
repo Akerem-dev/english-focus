709c18044d6de10902806d4fb34ae51169e02222
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-01 15:18:44 +0300

    fix(word-valley): bind final sidebar logo asset

--- apps/desktop/src/app/layout/AppSidebar.tsx
+++ apps/desktop/src/app/layout/AppSidebar.tsx
@@ -1,6 +1,6 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
 
-import wordValleyLogo from "../../assets/word-valley-final/shell/word-valley-logo.png";
+import wordValleyLogo from "../../assets/shell/word-valley-logo.png";
 import { SidebarNavItem } from "../../components/navigation";
 import { AppIcon, type AppIconName } from "../../design-system";
 import { dispatchAppCommand } from "../command-bar";
