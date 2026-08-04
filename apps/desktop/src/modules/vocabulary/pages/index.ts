88e8b36974a5e507b7020d9066ecf5f4453a6148
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-04 20:31:14 +0300

    fix(search): load truthful Wordie state styling

--- apps/desktop/src/modules/vocabulary/pages/index.ts
+++ apps/desktop/src/modules/vocabulary/pages/index.ts
@@ -2,5 +2,6 @@ import "../../../styles/vocabulary-search-results.css";
 import "../../../styles/vocabulary-detail.css";
 import "../../../styles/word-valley-figma-84.css";
 import "../../../styles/word-valley-figma-84-overrides.css";
+import "../../../styles/word-valley-figma-84-assistant-states.css";
 
 export { VocabularyPage } from "./VocabularyPage";
