757ba34311a50da4a9776c79343e619ef19e6e4e
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-04 19:06:39 +0300

    perf(search): load vocabulary result CSS with the route

--- apps/desktop/src/modules/vocabulary/pages/index.ts
+++ apps/desktop/src/modules/vocabulary/pages/index.ts
@@ -1,3 +1,5 @@
+import "../../../styles/vocabulary-search-results.css";
+import "../../../styles/vocabulary-detail.css";
 import "../../../styles/word-valley-figma-84-shell.css";
 import "../../../styles/word-valley-figma-84.css";
 
