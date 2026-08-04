f0778b8b52e8eb204d664588cecc5e277ca6feea
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-04 20:44:09 +0300

    feat(search): load functional result-card styles

--- apps/desktop/src/modules/vocabulary/pages/index.ts
+++ apps/desktop/src/modules/vocabulary/pages/index.ts
@@ -3,5 +3,6 @@ import "../../../styles/vocabulary-detail.css";
 import "../../../styles/word-valley-figma-84.css";
 import "../../../styles/word-valley-figma-84-overrides.css";
 import "../../../styles/word-valley-figma-84-assistant-states.css";
+import "../../../styles/word-valley-figma-84-result-states.css";
 
 export { VocabularyPage } from "./VocabularyPage";
