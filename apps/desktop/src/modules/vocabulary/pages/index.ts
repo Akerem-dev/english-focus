d48ffb0ae30c830170156098d3d0bd035ba0cff7
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-04 19:20:56 +0300

    fix(search): load final integration overrides last

--- apps/desktop/src/modules/vocabulary/pages/index.ts
+++ apps/desktop/src/modules/vocabulary/pages/index.ts
@@ -1,6 +1,6 @@
 import "../../../styles/vocabulary-search-results.css";
 import "../../../styles/vocabulary-detail.css";
-import "../../../styles/word-valley-figma-84-shell.css";
 import "../../../styles/word-valley-figma-84.css";
+import "../../../styles/word-valley-figma-84-overrides.css";
 
 export { VocabularyPage } from "./VocabularyPage";
