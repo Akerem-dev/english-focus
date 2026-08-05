8d40ce61a2997c45fa9e1d3a050e9fd4b10c1b50
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-05 23:06:48 +0300

    style(word-valley): load final asset polish last

--- apps/desktop/src/modules/vocabulary/pages/index.ts
+++ apps/desktop/src/modules/vocabulary/pages/index.ts
@@ -4,5 +4,6 @@ import "../../../styles/word-valley-figma-84.css";
 import "../../../styles/word-valley-figma-84-overrides.css";
 import "../../../styles/word-valley-figma-84-assistant-states.css";
 import "../../../styles/word-valley-figma-84-result-states.css";
+import "../../../styles/word-valley-figma-84-asset-polish.css";
 
 export { VocabularyPage } from "./VocabularyPage";
