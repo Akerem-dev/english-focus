24c54bab35318f71f6552de272bba225e655fda3
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-08-09 16:52:48 +0300

    feat(search): add rebuild module exports

---
+++ apps/desktop/src/modules/search-rebuild/index.ts
@@ -0,0 +1,4 @@
+export { SearchRebuildFoundView } from "./SearchRebuildFoundView";
+export type { SearchRebuildFoundViewProps } from "./SearchRebuildFoundView";
+export { SearchRebuildView } from "./SearchRebuildView";
+export type { SearchRebuildActivityItem, SearchRebuildSuggestion } from "./SearchRebuildView";
